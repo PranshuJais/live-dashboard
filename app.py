6876c2a0034764513db9946744098de48935240b
PranshuJais <pranshuj@thrillophilia.com>
2025-08-10 12:19:14 +0000

    add app + requirements

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSOpKvEzg9K2VkPRuI4hwFJYWWoszs7UCpZaSzEDmK8Rnf5LPksKLlWjc33peqrqi8lE4a49DWw21Bu/pub?output=csv"
+refresh_ms = 3000
+cache_ttl = 2
 
+date_column = ""                # No date column in your data
+revenue_column = "roi (%)"      # Or change to the metric you want as main value
+bookings_column = ""
+channel_column = "channel"
+experience_name_column = "campaign name"
+experience_id_column = ""
